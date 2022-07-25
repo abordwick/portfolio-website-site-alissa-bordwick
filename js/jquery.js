45df12ae193730eff8db27ea64dd6ff946d62927
abordwick <108829922+abordwick@users.noreply.github.com>
2022-07-25 16:31:38 -0400

    added jquery

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,10 @@
-console.log("Hey there, welcome to my portfolio site!")
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
-// Function that opens the menu when it is closed, and closes the menu when it is open
+
+.navtoggle li:not(:last-child) {
+  display: none;
+}
+
 function menuToggle () {
   var x = document.getElementById('mynavtoggle')
   // Below changes class name based on existing class name, so that the proper version of the menu (shown or hidden) will be displayed.
